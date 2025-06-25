4fb88fb12fc84f4fddb6d9a2745f5667ad13080a
Gilvanilson <15708579+gilvanilson-gil@users.noreply.github.com>
2025-06-25 16:37:53 -0300

    Create __manifest__.py

---
+++ dbfilter_header_traefik/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "dbfilter_header_traefik",
+    "summary": "Filter databases with HTTP headers, adapted for Traefik hostnames",
+    'version': "1.0.0",
+    "category": "Tools",
+    'author': "Impulse Team",
+    "website": "https://github.com/agenciaimpulse/odoo-impulse/dbfilter_header_traefik",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "auto_install": False,
+    "installable": True,
+}
